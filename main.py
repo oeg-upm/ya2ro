04da02822be4743c92e3581fa602ff95bac85018
PavelAntonia <floriana.antonia.pavel@gmail.com>
2021-09-20 12:33:52 +0300

    refactoring
    Links description added

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
 # import pyyaml module
-import yaml
+import yaml 
 from yaml.loader import SafeLoader
-
-# open the file and load the file
-with open('input.yaml') as file:
-    data = yaml.load(file, Loader=SafeLoader)
-
-# code to modify the file
+# import BeautifulSoup
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(open('template.html'), 'html.parser')
+# import RegEx
+import re
 
-# modify the title
-soup.find(id = "showcase").h1.string = data["title"]
+def append_items_link(category, ul_list):
+    for entry in data[category]:
+        li_new_tag = soup.new_tag('li')
+        a_new_tag = soup.new_tag('a')
 
-# modify the summary
-summary_content = soup.find(id = "summary-content").string = "Summary: " + data["summary"]
+        link_and_description = entry.split(';', 2) 
+        # 0: link, 1: link name, 2: description
+        a_new_tag['href'] = link_and_description[0]
+        a_new_tag.string = link_and_description[1]
+        li_new_tag.append(a_new_tag)
+        li_new_tag.a.insert_after(": " + link_and_description[2])
+        ul_list.append(li_new_tag)
 
-# modify the datasets
-datasets_list = soup.find(id="datasets-list")
 
-for link in data["datasets"]:
-    li_new_tag = soup.new_tag('li')
-    a_new_tag = soup.new_tag('a')
-    a_new_tag['href'] = link
-    a_new_tag.string = link
-    li_new_tag.append(a_new_tag)
-    li_new_tag.a.insert_after(": TODO Dataset description")
-    datasets_list.append(li_new_tag)
+if __name__ == "__main__":
 
-# modify software
-software_list = soup.find(id="software-list")
+    # open the file and load the file
+    with open('input.yaml') as file:
+        data = yaml.load(file, Loader=SafeLoader)
 
-for link in data["software"]:
-    li_new_tag = soup.new_tag('li')
-    a_new_tag = soup.new_tag('a')
-    a_new_tag['href'] = link
-    a_new_tag.string = "TODO software name"
-    li_new_tag.append(a_new_tag)
-    li_new_tag.a.insert_after(": TODO Software description")
-    software_list.append(li_new_tag)
+    # read and parse the template
+    soup = BeautifulSoup(open('template.html'), 'html.parser')
 
-#modify bibliography
-bibliography_list = soup.find(id="bibliography-list")
+    # create the title
+    soup.find(id = "showcase").h1.string = data["title"]
 
-for entry in data["bibliography"]:
-    li_new_tag = soup.new_tag('li')
-    li_new_tag.string = entry
-    bibliography_list.append(li_new_tag)
+    # create the summary
+    summary_content = soup.find(id = "summary-content")
+    summary_content.string = "Summary: " + data["summary"]
+
+    # create the datasets
+    datasets_list = soup.find(id="datasets-list")
+    append_items_link("datasets", datasets_list)
+
+    # create software
+    software_list = soup.find(id="software-list")
+    append_items_link("software", software_list)
+        
+    # create bibliography
+    bibliography_list = soup.find(id="bibliography-list")
+
+    for entry in data["bibliography"]:
+        li_new_tag = soup.new_tag('li')
+        li_new_tag.string = entry
+        bibliography_list.append(li_new_tag)
+
+    # create authors
+    about_authors = soup.find(id="about_authors")
     
-# to save the changes
-with open("index.html", "w") as file:
-    file.write(str(soup))
+        
+    # to save the changes
+    with open("index.html", "w") as file:
+        file.write(str(soup))
 
