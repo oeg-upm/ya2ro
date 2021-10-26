56ac047a2d8c9a08fde5aafe6277b86867b509cd
PavelAntonia <floriana.antonia.pavel@gmail.com>
2021-10-26 14:55:14 +0300

    Updated HTML components and sidebar

--- Ro_html.py
+++ Ro_html.py
@@ -42,34 +42,66 @@ class Ro_html(object):
     
     def init_social_motivation(self, social_motivation):
 
-        sc_component = f"""<div class="w3-container" style="margin-top:15px">
-		<h1 class="w3-xxxlarge w3-text-green"><b>Social Motivation.</b></h1>
+        social_motivation_component = f"""<div class="w3-container" style="margin-top:15px">
+		<h1 class="w3-xxxlarge w3-text-green"><b>Social Motivation</b></h1>
         <hr style="width:50px;border:5px solid green" class="w3-round">
 		<p>{social_motivation}</p>
 	    </div>"""
 
-        self.__append_component("social_motivation", sc_component)
+        self.__append_component("social_motivation", social_motivation_component)
+        self.__sidebar_append("social_motivation", "Social motivation")
     
     def init_sketch(self, sketch):
         
-        sketch_component = f"<p>[This is a sketch: {sketch}]</p>"
+        sketch_component = f"""<div class="w3-container" style="margin-top:15px">
+		<h1 class="w3-xxxlarge w3-text-green"><b>Sketch</b></h1>
+        <hr style="width:50px;border:5px solid green" class="w3-round">
+		<img src="{sketch}" alt="Sketch of the project">
+	    </div>"""
         self.__append_component("sketch", sketch_component)
+        self.__sidebar_append("sketch", "Sketch")
+
+
+        # copy image to output/images directory
+        src = Path(sketch)
+        dst = Path(p.output_directory + "/" + sketch)
+        copyfile(src, dst)
     
     def init_areas(self, areas):
 
-        areas_component = f"<p>[This is a list of areas: {areas}</p>"
+        areas_list = self.__make_html_ul_component(areas)
+
+        areas_component = f"""<div class="w3-container" style="margin-top:15px">
+		<h1 class="w3-xxxlarge w3-text-green"><b>Areas</b></h1>
+        <hr style="width:50px;border:5px solid green" class="w3-round">
+		{areas_list}
+	    </div>"""
         self.__append_component("areas", areas_component)
+        self.__sidebar_append("areas", "Areas")
     
     def init_demo(self, demo):
         
-        demo_component = f"<p>[This is a link to a demo: {demo}</p>"
+        demo_component = f"""<div class="w3-container" style="margin-top:15px">
+		<h1 class="w3-xxxlarge w3-text-green"><b>Demo</b></h1>
+        <hr style="width:50px;border:5px solid green" class="w3-round">
+		<p><a href="{demo}">{demo}</a></p>
+	    </div>"""
+
         self.__append_component("demo", demo_component)
+        self.__sidebar_append("demo", "Demo")
 
 
     def init_goal(self, goal):
-        # create the goal
-        summary_content = self.soup.find(id = "summary-content")
-        summary_content.string = "Goal: " + goal
+
+        goal_component = f"""<div class="w3-container" style="margin-top:15px">
+		<h1 class="w3-xxxlarge w3-text-green"><b>Goal</b></h1>
+        <hr style="width:50px;border:5px solid green" class="w3-round">
+		<p>{goal}</p>
+	    </div>"""
+
+        self.__append_component("goal", goal_component)
+        self.__sidebar_append("goal", "Goal")
+ 
 
     def init_title(self, title):
         # modify web title metadata
@@ -78,9 +110,15 @@ class Ro_html(object):
         self.soup.find(id = "showcase").h1.string = title
     
     def init_summary(self, summary):
-        # create the summary
-        summary_content = self.soup.find(id = "summary-content")
-        summary_content.string = "Summary: " + summary
+
+        summary_component = f"""<div class="w3-container" style="margin-top:15px">
+		<h1 class="w3-xxxlarge w3-text-green"><b>Goal</b></h1>
+        <hr style="width:50px;border:5px solid green" class="w3-round">
+		<p>{summary}</p>
+	    </div>"""
+
+        self.__append_component("summary", summary_component)
+        self.__sidebar_append("summary", "Summary")
     
     def init_doi_datasets(self, doi_dataset):
         # Insert DOI link
@@ -94,11 +132,13 @@ class Ro_html(object):
         # Insert list of datasets
         datasets_list = self.soup.find(id="datasets-list")
         self.__append_items_link(datasets, datasets_list)
+        self.__sidebar_append("datasets", "Datasets")
     
     def init_software(self, software):
         # create software
         software_list = self.soup.find(id="software-list")
         self.__append_items_link(software, software_list)
+        self.__sidebar_append("software", "Software")
     
     def init_bibliography(self, bibliography):
         # create bibliography
@@ -107,14 +147,16 @@ class Ro_html(object):
             li_new_tag = self.soup.new_tag('li')
             li_new_tag.string = entry.entry
             bibliography_list.append(li_new_tag)
+        self.__sidebar_append("bibliography", "Bibliography")
 
     def init_authors(self, authors):
         # create authors
         about_authors = self.soup.find(id="about_authors")
         self.__create_about_authors(authors, about_authors)
+        self.__sidebar_append("authors", "About the authors")
 
         # copy images to output/images directory
-        for author in p.data.authors:
+        for author in authors:
 
             src = Path(author.photo)
             dst = Path(p.output_directory + "/" + author.photo)
@@ -179,3 +221,14 @@ class Ro_html(object):
         loc = self.soup.find(id=location_id)
         html_component = BeautifulSoup(str_component, 'html.parser')
         loc.append(html_component)
+    
+    def __sidebar_append(self, location_id, item_name):
+        item_component = f"""<a href="#{location_id}" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">{item_name}</a>"""
+        self.__append_component("sidebar", item_component)
+
+    def __make_html_ul_component(self, list):
+        ul_component = """<ul>"""
+        for li in list:
+            ul_component += f"""<li>{li}</li>"""
+        ul_component += """</ul>"""
+        return ul_component
