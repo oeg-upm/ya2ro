f29482a48204b96fca28cbdfdf200705bc10c4e5
PavelAntonia <floriana.antonia.pavel@gmail.com>
2021-11-20 13:44:14 +0200

    req_orcid more robust

--- req_orcid.py
+++ req_orcid.py
@@ -7,28 +7,44 @@ class orcid(object):
         self.json = json.loads(req.get(orcid_link, headers={"Accept":"application/ld+json"}).text)
         self.json_bio = json.loads(req.get(orcid_link, headers={"Accept":"application/orcid+json"}).text)
 
-    def get_full_name(self)-> str:
-        return self.json["givenName"]+" "+self.json["familyName"]
-    
-    def get_webs(self):
-        """Get all webs separated in a list'"""
-        if self.json["url"] is None:
+    def get_full_name(self):
+        try:
+            return self.json["givenName"]+" "+self.json["familyName"]
+        except:
             return None
-        else:
-            return list(self.json["url"])
-    
+            
+    def get_webs(self):
+        """Get all webs separated in a list"""
+        try:
+            if self.json["url"] is None:
+                return None
+            else:
+                return list(self.json['url'])
+        except:
+            return None
 
     def get_affiliation(self):
         """Gets all the non repeated names afiliation in a list '"""
-        affiliations = set()
+        try:
+            affiliations = set()
 
-        if self.json["affiliation"] is None:
+            if self.json["affiliation"] is None:
+                return None
+
+            if isinstance(self.json["affiliation"], dict):
+                affiliations.add(self.json["affiliation"]['name'])
+
+            if isinstance(self.json["affiliation"], list):
+                for aff in self.json["affiliation"]:
+                    affiliations.add(aff['name'])
+            
+            return list(affiliations)
+
+        except:
             return None
-
-        for aff in self.json["affiliation"]:
-            affiliations.add(aff["name"])
         
-        return list(affiliations)
-    
     def get_bio(self):
-        return self.json_bio["person"]["biography"]["content"]
+        try:
+            return self.json_bio['person']['biography']['content']
+        except:
+            return None        
