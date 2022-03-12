00127e176044839f938b2e94354a9d413d57c391
Antonia Pavel <floriana.antonia.pavel@gmail.com>
2022-03-12 19:54:01 +0100

    Refactor configuration

--- src/ya2ro/properties.py
+++ src/ya2ro/properties.py
@@ -1,7 +1,7 @@
 import yaml 
 from yaml.loader import SafeLoader
 from pathlib import Path
-
+import os
 
 def init(properties_file, output_directory_param, n_somef):
     global output_directory, input_to_vocab, properties, style, base_dir, no_somef
@@ -27,13 +27,37 @@ def init(properties_file, output_directory_param, n_somef):
     style = _safe(input_to_vocab["style"], properties)
     style = style if style else "default"
 
+    configure_somef(properties_file)
+
     print(f"{style} theme selected.")
     print("Properties initialized...")
+
+def configure_somef(properties_file):
+
+    if os.path.isfile(Path(base_dir)/'resources'/'is_somef_configured'):
+        return
+
+    from somef import configuration
+    print("Running SOMEF initial configuration:")
+    token = _safe('GITHUB_PERSONAL_ACCESS_TOKEN', properties)
+    if token:
+        configuration.configure(authorization=token)
+        with open(Path(base_dir)/'resources'/'is_somef_configured', 'w+') as f:
+            f.write('Yes :D')
+    else:
+        print(
+            f"""
+WARNING: GITHUB_PERSONAL_ACCESS_TOKEN is not configured. Please add your personal token in ya2ro configuration file.
+        --> {Path(base_dir, properties_file)} <--
+
+        ADD a line like the following:
+        GITHUB_PERSONAL_ACCESS_TOKEN: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+        ya2ro will continue working, but is highly recommended to apply this setting.
+            """
+        )
 
 
 def _safe(key, dic):
     """Safe call to a dictionary. Returns value or None if key or dictionary does not exist"""
-    if dic is not None and key in dic:
-        return dic[key]
-    else:
-        return None
+    return dic[key] if dic and key in dic else None
--- src/ya2ro/ya2ro.py
+++ src/ya2ro/ya2ro.py
@@ -36,10 +36,6 @@ def main():
     group.add_argument('-i','--input', type=str, metavar='YALM_PATH',
         help='Path of the required yaml input. Follow the documentation or the example given to see the structure of the file.')
 
-    # Required positional argument 
-    group.add_argument('-c','--configure', type=str, metavar='GITHUB_PERSONAL_ACCESS_TOKEN',
-        help='Insert Github personal access token. Run this the first time to configure ya2ro properly.')
-
     # Required positional argument
     group.add_argument('-l','--landing_page', type=str, metavar='YA2RO_PREV_OUTPUT',
         help='Path of a previous output folder using the ya2ro tool. This flag will make a landing page to make all the resources accesible.')
@@ -57,10 +53,6 @@ def main():
          help='Disable SOMEF for a faster execution (software cards will not work).')
 
     args = parser.parse_args()
-    
-    if args.configure:
-        configure_somef(args.configure)
-        return
 
     from . import properties
 
@@ -76,9 +68,7 @@ def main():
     if args.landing_page:
         process_landing_page(args.landing_page)
 
-
-
-
+        
 def process_yaml(yaml_folder_or_file_str):
 
     try:
@@ -131,11 +121,6 @@ def process_landing_page(landing_page_directory):
 
     rlanding = ro_landing_page(landing_page_directory)
     rlanding.create_landing_page()
-    
-def configure_somef(token):
-    from somef import configuration
-    print("Running initial configuration:")
-    configuration.configure(authorization=token)
 
 if __name__ == "__main__":
     main()
